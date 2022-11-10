bec00c58fdf04194ff4813e6053d01fadfff1fee
black6765 <black6765@gmail.com>
2022-11-10 15:54:08 +0900

    Update to 3-06

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,12 +3,15 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/question")
@@ -19,9 +22,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @RequestMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
@@ -41,8 +44,8 @@ public class QuestionController {
         return "redirect:/question/list";
     }
 
-    @GetMapping("/create")
-    public String questionCreate(QuestionForm questionForm) {
-        return "question_form";
-    }
+//    @GetMapping("/create")
+//    public String questionCreate(QuestionForm questionForm) {
+//        return "question_form";
+//    }
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,24 +19,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Autowired
-	private QuestionRepository questionRepository;
+//	@Autowired
+//	private QuestionRepository questionRepository;
+//
+//	@Autowired
+//	private AnswerRepository answerRepository;
 
 	@Autowired
-	private AnswerRepository answerRepository;
+	private QuestionService questionService;
 
-	@Transactional
 	@Test
 	void testJpa() {
 
-		Optional<Question> oa = questionRepository.findById(2);
-		assertTrue(oa.isPresent());
-		Question q = oa.get();
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 
-		List<Answer> list = q.getAnswerList();
-
-		assertEquals(1, list.size());
-		assertEquals("네 자동으로 생성됩니다.", list.get(0).getContent());
+//		Optional<Question> oa = questionRepository.findById(2);
+//		assertTrue(oa.isPresent());
+//		Question q = oa.get();
+//
+//		List<Answer> list = q.getAnswerList();
+//
+//		assertEquals(1, list.size());
+//		assertEquals("네 자동으로 생성됩니다.", list.get(0).getContent());
 
 //		Optional<Answer> oa = this.answerRepository.findById(1);
 //		assertTrue(oa.isPresent());
